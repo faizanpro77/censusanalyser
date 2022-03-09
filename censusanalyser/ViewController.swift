6edec14fbe82269159ddc6bd320fc6878a12fc1d
faizanpro77 <faizyshaikh204@gmail.com>
2022-03-09 15:27:51 +0530

    add census analyser

--- censusanalyser/ViewController.swift
+++ censusanalyser/ViewController.swift
@@ -7,13 +7,110 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+
+
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Do any additional setup after loading the view, typically from a nib.
+
+        
+
+        // Save data to file
+
+        let fileName = "Test"
+
+        let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+
+        
+
+        let fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("txt")
+
+        print("FilePath: \(fileURL.path)")
+
+        
+
+        let writeString = "Write this text to the fileURL as text in iOS using Swift"
+
+        do {
+
+            // Write to the file
+
+            try writeString.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+
+        } catch let error as NSError {
+
+            print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
+
+        }
+
+     
+
+        var readString = "" // Used to store the file contents
+
+        do {
+
+            // Read the file contents
+
+            readString = try String(contentsOf: fileURL)
+
+        } catch let error as NSError {
+
+            print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
+
+        }
+
+        print("File Text: \(readString)")
+
+        
+
+        /*** Read from project txt file ***/
+
+        
+
+        // File location
+
+        let fileURLProject = Bundle.main.path(forResource: "ProjectTextFile", ofType: "txt")
+
+        // Read from the file
+
+        var readStringProject = ""
+
+        do {
+
+            readStringProject = try String(contentsOfFile: fileURLProject!, encoding: String.Encoding.utf8)
+
+        } catch let error as NSError {
+
+             print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
+
+        }
+
+
+
+        print(readStringProject)
+
+        
+
+        
+
+        
+
+        
+
     }
 
+    override func didReceiveMemoryWarning() {
 
+        super.didReceiveMemoryWarning()
+
+        // Dispose of any resources that can be recreated.
+
+    }
 }
 
